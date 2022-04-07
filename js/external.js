d84ebb654092dd81049153c7ac22fefb78990abe
“Kenneth <Pittman.Kenny24@gmail.com>
2022-04-07 12:15:10 -0500

    changed file due to errors

--- js/external.js
+++ js/external.js
@@ -1,4 +1,12 @@
+"use strict";
 console.log('Hello, from external JavaScript')
 alert(`Welcome to my Website!`)
-var favoriteColor = prompt(message: 'What is your favorite color?')
-    alert('My favorite color is also' + favoriteColor)
+
+var favoriteColor = parseFloat(prompt('What is your favorite color?'))
+    alert('My favorite color is also' + favoriteColor);
+
+var daysRentedLittleMermaid = parseFloat(prompt( 'How many days was the Little Mermaid rented'))
+var daysRentedBrotherBear = parseFloat(prompt(  'How many days was Brother Bear rented?'))
+var daysRentedHurcules = parseFloat(prompt( 'How many days was Hurcules rented?'))
+var totalcost = ((daysRentedLittleMermaid + daysRentedBrotherBear + daysRentedHurcules) *3)
+alert('Total days rented charge' + totalcost)
