7f256d1261e2564d1227e26ef29895d431c6f017
“Kenneth <Pittman.Kenny24@gmail.com>
2022-04-07 09:12:37 -0500

    js/external.js

---
+++ js/external.js
@@ -0,0 +1,4 @@
+console.log('Hello, from external JavaScript')
+alert(`Welcome to my Website!`)
+var favoriteColor = prompt(message: 'What is your favorite color?')
+    alert('My favorite color is also' + favoriteColor)
