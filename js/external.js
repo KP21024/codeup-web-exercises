633e6504be577506dff3f517fe07535b9a90f2be
“Kenneth <Pittman.Kenny24@gmail.com>
2022-04-07 13:57:58 -0500

    updaded more of the exercise

--- js/external.js
+++ js/external.js
@@ -11,3 +11,15 @@ var daysRentedHurcules = parseFloat(prompt( 'How many days was Hurcules rented?'
 var totalcost = ((daysRentedLittleMermaid + daysRentedBrotherBear + daysRentedHurcules) *3)
 alert('Total days rented charge' + totalcost)
 
+var googleHourlyRateDollars = parseFLoat (prompt ( "What is your Google hourly rate?"))
+var amazonHourlyRateDollars = parseFloat (prompt ( "What is your Amazon hourly rate?"))
+var facebookHourlyRateDollars = parseFloat (prompt ( "What is your Facebook hourly rate?"))
+
+var googleHours = parseFloat (prompt ("How many hours this week at GoogLe?"))
+var amazonHours = parseFloat (prompt ( "How many hours this week at Amazon?"))
+var facebookHours = parseFLoat (prompt ("How many hours this week at Facebook?"))
+
+var totalPayment = googleHours * googleHourlyRateDollars
+totalPayment += amazonHours * amazonHourlyRateDollars
+totalPayment += facebookHours * facebookHourlyRateDollars
+alert("Total payment is $" + totalPayment + totalPayment.toFixed (2))
