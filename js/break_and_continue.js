97ff7bc2a8338130cea5ed3d91d6c0903ab305e2
“Kenneth <Pittman.Kenny24@gmail.com>
2022-04-13 17:03:34 -0500

    updated breaks an continue, started arrays HW

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -5,8 +5,28 @@ function getOddNumber(max) {
 
     while (isInputInvalid) {
 
-        if parseInt(input) % 2 !== 0 && parseInt(input) <= max)) break;
+    //    if parseInt(input) % 2 !== 0 && parseInt(input) <= max)) break;
 
-        input = prompt("Input odd number less than " + max");
+  //      input = prompt("Input odd number less than " + max");
     }
+//}
+
+function doubleNumber(start, max) {
+
+    while (start <= max) {
+        console.log(start);
+        start = start*2;
+    }
+}}
+
+fucntion calculateConeSales(totalCones) {
+
+    do {
+        var totalToSell = generateRandomSales();
+
+        console.log("5 cones sole...")
+
+        console.log("Cannot sell you 6 cones I only have 3...")
+
+    }   while (totalCones <= 0);
 }
