e92358dd78c8a58d7cab82ad3f79a9e325483c6f
“Kenneth <Pittman.Kenny24@gmail.com>
2022-04-14 16:42:21 -0500

    updated planets-string

--- js/planets-string.js
+++ js/planets-string.js
@@ -1,8 +1,12 @@
 (function(){
     "use strict";
 
-    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray = ["Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune"];
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune"
+    var planetsArray;
+    planetsArray = planetsString.split("|");
+    console.log(planetsArray);
+
+
 
 
     /**
@@ -12,14 +16,14 @@
      * console.log planetsArray to check your work
      */
 
-     // console.log(planetsArray);
-    var brString = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
-    console.log(brString)
+      // console.log(planetsArray);
+    //
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
-     var brString = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
+
+      var brString = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
 
      *
      * BONUS:
@@ -27,5 +31,10 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+    var planets2 = planetsArray.join('br');
+    console.log(planets2);
+    var planetsList = '<ul><li>' + planetsArray.join('</li><li>') + '<li></ul>';
+    console.log(planetsList);
 
-})();
+    // var unorderedList = <ul><li> + "Mercury"<li>Venus<li>Earth<li>Mars<li>Jupiter<li>Saturn<li>Uranus<li>Neptune<li>"</ul>
+ })();
