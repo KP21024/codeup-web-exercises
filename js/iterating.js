cc6585a641a6711b3529593315dca9dde1bcedbe
“Kenneth <Pittman.Kenny24@gmail.com>
2022-04-14 00:59:40 -0500

    finished arrays exercise and some of the extra work

--- js/iterating.js
+++ js/iterating.js
@@ -1,34 +1,62 @@
-(function(){
+
     "use strict";
+        // var names = ['Bob', 'Dave', 'Jack', 'Frank'];
+
 
     /**
      * TODO:
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+        // console.log(names.length);
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+        // console.log(names)[0];
+        // console.log(names)[1];
+        // console.log(names)[2];
+        // console.log(names)[3];
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    // var names = ['Bob', 'Dave', 'Jack', 'Frank'];
+    // {
+    //     for (var i = 0; i < names.length; i++) {
+    //         console.log('The shape at index ' + i + ' is: ' + names[i]);
+    //     }
+    // }
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+    // var names = ['Bob', 'Dave', 'Jack', 'Frank'];
+    // {
+    //     names.forEach(function (name) {
+    //         console.log('Here is a cool name: ' + name + '.');
+    //     });
+    // }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+    var firstSecondLast = ['1', '2', '3']
+    function first() {
+        return [0];
+    }
+    function second() {
+        return [1];
+    }
+    function last() {
+        return [2];
+
+    }
+
+
 
     /**
      * TODO:
@@ -43,5 +71,3 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
-
-})();
