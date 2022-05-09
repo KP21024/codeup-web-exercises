41cefb40e616fe12aec4af7fff5be2043992ae8f
“Kenneth <Pittman.Kenny24@gmail.com>
2022-05-09 15:41:42 -0500

    completed while.js and flexbox fundamentals

--- js/while.js
+++ js/while.js
@@ -1,38 +1,49 @@
-(function(){
-    'use strict'
-    var i = 2;
-    while(i <= 65536){
-        console.log(i)
-        i *= 2
+// Create a while loop that uses console.log() to create the output shown below.
+//
+//
+// 2
+// 4
+// 8
+// 16
+// 32
+// 64
+// 128
+// 256
+// 512
+// 1024
+// 2048
+// 4096
+// 8192
+// 16384
+// 32768
+// 65536
+
+    var myVar = 2;
+    while (myVar < 65536) {
+        console.log(myVar);
+        myVar *= 2;
     }
-    //     // This is how you get a random number between 50 and 100
-    var allCones = Math.floor(Math.random() * 50) + 50;
-//     console.log(i)
-//     do{
-// // // This expression will generate a random number between 1 and 5
-//       var conesBought = Math.floor(Math.random() * 5) + 1;all
-    function randomSales(){
-        return  Math.floor(Math.random() * 5) + 1;
-    }
-    function calculateCones(totalCones) {
-        do {
-            var totalToSell = Math.floor(Math.random() * 50) + 50;
+// Do While Loop
+// An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. The below code shows how to get the random numbers for this exercise.
+//
+//
+// // This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+    do {
+        var randomConesSold = Math.floor(Math.random() * 5 + 1);
+        if (allCones - randomConesSold >= 0) {
+            allCones -= randomConesSold;
+            console.log("Sold " + randomConesSold + "! I have " + allCones + " left!");
 
-            var actualSoldAmount = (totalCones >= totalToSell) ? totalToSell : totalCones;
-
-            if (totalToSell === actualSoldAmount) {
-                console.log(actualSoldAmount + ' Amount of cones sold...')
-            } else {
-                console.log('can not sell you ' + totalToSell + ' cones I only have ' + actualSoldAmount + '...')
-            }
-            totalCones -= actualSoldAmount
-
-        } while (totalCones > 0)
-        console.log('YES! I sold them all!')
-    }
-    calculateCones(allCones)
-
-
-
-
-})();
+        } else {
+            console.log("I cannot sell you " + randomConesSold + ", I only have " + allCones + " left.");
+        }
+    } while (allCones > 0);
+// // This expression will generate a random number between 1 and 5
+// Math.floor(Math.random() * 5) + 1;
+// The output should be similar to the following:
+//
+//     5 cones sold...  // if there are enough cones
+// Cannot sell you 6 cones I only have 3...  // If there are not enough cones
+// Yay! I sold them all! // If there are no more cones
+//     1. Finally, commit the changes to your git repository, and push to GitHub.
