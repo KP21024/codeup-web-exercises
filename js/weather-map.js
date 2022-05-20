651b82a7ef4d46e11c865727fd19e68f3eabde18
“Kenneth <Pittman.Kenny24@gmail.com>
2022-05-20 16:27:18 -0500

    weather-map

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,9 +1,11 @@
 // $.get("http://api.openweathermap.org/data/2.5/weather", {
 //     APPID: OPEN_WEATHER_APPID,
-//     q:     "San Antonio, US"
+//     q:     "San Antonio, US",
+//     units: "imperial"
 // }).done(function(data) {
 //     console.log(data);
 // });
+
 
 $.get("http://api.openweathermap.org/data/2.5/onecall", {
     APPID: OPEN_WEATHER_APPID,
@@ -13,5 +15,18 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
 }).done(function(data) {
     console.log('The entire response:', data);
     console.log('Diving in - here is current information: ', data.current);
+    $("#current-weather").append("<p>" + data.current.temp + "</p>")
     console.log('A step further - information for tomorrow: ', data.daily[1]);
+    data.daily.forEach(function (dailyForcast, index){
+        if (index < 5 ){
+            console.log(new Date(dailyForcast.dt*1000));
+            console.log(dailyForcast)
+            $("#forcast").append("<p>" + new Date(dailyForcast.dt*1000) + "</p>")
+            $("#forcast").append("<p>" + dailyForcast.temp.max + "</p>")
+
+        }
+
+        let current_datetime = new Date()
+        console.log(current_datetime.toString())
+    })
 });
