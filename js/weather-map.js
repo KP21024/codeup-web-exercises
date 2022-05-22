3c800abc05b1064f9057b4aa911fb14316fdbbd0
“Kenneth <Pittman.Kenny24@gmail.com>
2022-05-21 22:06:02 -0500

    worked on weather map with classmates

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,11 +1,20 @@
 // $.get("http://api.openweathermap.org/data/2.5/weather", {
 //     APPID: OPEN_WEATHER_APPID,
-//     q:     "San Antonio, US",
-//     units: "imperial"
+//     q:     "San Antonio, US"
 // }).done(function(data) {
 //     console.log(data);
 // });
 
+// $.get("http://api.openweathermap.org/data/2.5/onecall", {
+//     APPID: OPEN_WEATHER_APPID,
+//     lat:    29.423017,
+//     lon:   -98.48527,
+//     units: "imperial"
+// }).done(function(data) {
+//     console.log('The entire response:', data);
+//     console.log('Diving in - here is current information: ', data.current);
+//     console.log('A step further - information for tomorrow: ', data.daily[1]);
+// });
 
 $.get("http://api.openweathermap.org/data/2.5/onecall", {
     APPID: OPEN_WEATHER_APPID,
@@ -29,4 +38,8 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
         let current_datetime = new Date()
         console.log(current_datetime.toString())
     })
+    data.daily.forEach(function (dailyHumidity,index) {
+
+        }
+    )
 });
